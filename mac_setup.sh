7c4768cce69a165cc47b92a50b213dc8d16f7c97
Alex Zamai <zamaialexander@gmail.com>
2017-07-14 17:08:41 +0200

    added comments

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+
+# OS X settings
 defaults write NSGlobalDomain KeyRepeat -int 0
 defaults write com.apple.dock autohide-time-modifier -int 0
 defaults write com.apple.finder AppleShowAllFiles YES
 killall Finder
 sudo spctl --master-disable
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Development
+brew install python3
+# install ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 brew cask install iterm2
-brew cask install skype
 brew cask install pycharm-ce
-brew install python3
 brew cask install sublime-text
 brew cask install docker
-brew cask install spotify
+
+# Create symlink for the subl
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 defaults write com.apple.LaunchServices LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.3;}'
 
@@ -24,5 +29,7 @@ gcloud components install kubectl
 
 # OPTIONAL: 
 # brew cask install sequel-pro
+#brew cask install spotify
 # brew cask install hipchat
-# brew cask install slack
+brew cask install slack
+brew cask install skype
