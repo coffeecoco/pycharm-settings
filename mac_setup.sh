b441b37876c98cb638d51793e50fab095aac6fed
Alex Zamai <zamaialexander@gmail.com>
2017-08-01 06:17:36 +0200

    a

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# OS X settings
+## OS X settings
+# faster keyboard:
 defaults write NSGlobalDomain KeyRepeat -int 0
 defaults write com.apple.dock autohide-time-modifier -int 0
+# show all files
 defaults write com.apple.finder AppleShowAllFiles YES
 killall Finder
 sudo spctl --master-disable
@@ -20,11 +22,11 @@ $ git config --global --unset-all user.email
 # Update Ruby
 \curl -sSL https://get.rvm.io | bash -s stable
 
+
+# intsall Dev Software
 brew install python3
 brew cask install iterm2
-brew cask install skype
-brew cask install slack
-brew cask install pycharm-ce
+brew cask install pycharm
 brew cask install sublime-text
 brew cask install postman
 brew cask install psequel
@@ -34,11 +36,6 @@ brew cask install docker
 ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 defaults write com.apple.LaunchServices LSHandlers -array-add '{LSHandlerContentType=public.plain-text;LSHandlerRoleAll=com.sublimetext.3;}'
 
-# Require setting user.name and email per-repo:
-$ git config --global user.useConfigOnly true
-# Remove email address from global config:
-$ git config --global --unset-all user.email
-
 # install gcloud SDK: /Applications
 curl https://sdk.cloud.google.com | bash
 source ~/.zshrc
@@ -47,7 +44,5 @@ gcloud components install kubectl
 
 # OPTIONAL:
 # brew cask install sequel-pro
-#brew cask install spotify
-# brew cask install hipchat
 brew cask install slack
 brew cask install skype
